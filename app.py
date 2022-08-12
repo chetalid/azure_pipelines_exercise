4340bc2e1fc60b222ca9be7ab05108dbec0bb8b4
chetalid <dchaitali11@gmail.com>
2022-08-12 19:03:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask.logging import create_logger
 import logging
-
+from markupsafe import Markup
 import pandas as pd
 from sklearn.externals import joblib
 from sklearn.preprocessing import StandardScaler
